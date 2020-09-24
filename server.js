cacc0bb1c97800400650940cc073b3e9bf4f7d1e
Amritanshu Dev Rawat <amritanshu360@gmail.com>
2020-09-24 23:41:22 +0530

    added gitignore and procfile and updated port

--- server.js
+++ server.js
@@ -59,6 +59,12 @@ app.get('/:id',(req,res)=>{
     }
   })
 })
-app.listen(3000,()=>{
-  console.log('Server started at 3000')
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port,()=>{
+  console.log(`Server started at ${port}`)
 })
